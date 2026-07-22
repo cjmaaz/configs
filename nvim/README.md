1d041257af09862c12455d78477163404f58d6b9
Maaz Rahman <maaz@techie.com>
2026-07-22 12:22:58 +0530

    init nvim config

---
+++ nvim/README.md
@@ -0,0 +1,11 @@
+# Pacman
+
+```bash
+sudo pacman -S --noconfirm --needed gcc make git ripgrep fd tree-sitter-cli unzip neovim
+```
+
+### Clipboard
+
+```bash
+sudo pacman -S wl-clipboard
+```
---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+
+
+  vim.g.have_nerd_font = true
+  
+  vim.o.relativenumber = true
+
+  vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+  vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+  vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+  vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+  
